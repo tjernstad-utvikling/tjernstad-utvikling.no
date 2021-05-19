4309a235b7ac67a97b391e978dac6947d95c78cf
Ole Tjernstad <ott556@gmail.com>
2021-05-19 14:31:26 +0200

    update versions

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,70 +1,45 @@
-/* eslint-disable no-labels */
-/* eslint-disable no-unused-expressions */
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'tjernstad-utvikling',
-    htmlAttrs: {
-      lang: 'no',
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'tjernstad-utvikling',
+        htmlAttrs: {
+            lang: 'no'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     },
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      { hid: 'description', name: 'description', content: '' },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/sanity/module'
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: ['~/plugins/sanity-image-builder.js'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/sanity',
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: ['nuxt-svg-loader'],
-  purgeCSS: {
-    whitelist: ['hidden'],
-    whitelistPatterns: [/md:w-[1-6]/],
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(_config, _ctx) {
-      loaders: {
-        file: {
-          esModule: false
-        }
-      }
-    },
-  },
-}
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {}
+};
