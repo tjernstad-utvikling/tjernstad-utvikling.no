71cd66693702c9f3b901c5d9321262da6038746a
Ole Tjernstad <ott556@gmail.com>
2021-05-24 21:47:31 +0200

    logo in footer

--- layout/components/footer.tsx
+++ layout/components/footer.tsx
@@ -1,6 +1,11 @@
 export const Footer = () => {
     return (
         <footer>
+            <img
+                className="footer-logo"
+                src="/img/tu-logo.svg"
+                style={{ width: '150px' }}
+            />
             <address>
                 <span> Tjernstad Utvikling </span> <br />
                 <span style={{ fontSize: '11px' }}>C/O Ole Tjernstad</span>
