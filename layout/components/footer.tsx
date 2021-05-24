4211541f311aef3400feb6e0ff9399d178111d04
Ole Tjernstad <ott556@gmail.com>
2021-05-24 19:44:07 +0200

    footer text

--- layout/components/footer.tsx
+++ layout/components/footer.tsx
@@ -1,8 +1,18 @@
 export const Footer = () => {
     return (
         <footer>
-            <address>Ole Tjernstad Sandervegen 590 2116 Sander</address>
-            <address>Tlf: 483 13 283 Org. nr. 924 359 595</address>
+            <address>
+                <span> Tjernstad Utvikling </span> <br />
+                <span style={{ fontSize: '11px' }}>C/O Ole Tjernstad</span>
+                <br />
+                <br />
+                Sandervegen 590
+                <br /> 2116 Sander
+            </address>
+            <address>
+                Tlf: 483 13 283 <br />
+                <br /> Org. nr. 924 359 595
+            </address>
         </footer>
     );
 };
